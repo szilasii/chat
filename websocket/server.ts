62da3d1604960890017c5012fe5a646815f807ef
szilasii <szilasii78@gmail.com>
2024-02-08 13:40:12 +0100

    add vuejs2

---
+++ websocket/server.ts
@@ -0,0 +1,27 @@
+
+import Websocket from 'ws'; 
+const clients = new Set();
+
+const server = new Websocket.Server({ port: 8081})
+server.on ('connection',(socket:Websocket) => {
+
+    clients.add(socket)
+
+    socket.on('message', (message)=> {
+            broadcast(message,socket)
+        })
+
+   
+    socket.on('close',() => {
+        clients.delete(socket)
+    })
+})
+
+function broadcast(message:any,sender:any) {
+    let client :any;
+    for (client of clients) {
+       // if (client != sender) {
+            client.send(message)
+       // }
+    }
+}
